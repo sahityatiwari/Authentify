4a0b361e1fabf19e0f37dada42a3eec35020001b
sahityatiwari <sahityatiwari90035@gmail.com>
2024-07-03 19:00:11 +0530

    9th

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,21 +1,23 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
-import '../App.css'; // Assuming you have a separate CSS file for styling
+import "../App.css"; // Import the CSS file for styling
 
 export default function Dashboard() {
     const { user } = useContext(UserContext);
 
     return (
         <div className="dashboard-container">
-            <h1 className="dashboard-title">Dashboard</h1>
-            {!!user && (
+            <div className="dashboard-content">
+                <h1 className="dashboard-title">Dashboard</h1>
                 <div className="dashboard-welcome">
-                    <h2 className="dashboard-welcome-message">Hi {user.name}!</h2>
+                    {!!user && (
+                        <h2 className="dashboard-welcome-message">Hi {user.name}!</h2>
+                    )}
                     <p className="dashboard-project-description">
-                        About Full-Stack Authentication with MERN and JWT: Demonstrates robust user authentication using MongoDB, Express.js, React, and Node.js, enhanced with JSON Web Tokens (JWT). Features include secure registration, login, and protected routes, ideal for building secure, scalable web applications.
+                        <strong>About:</strong> Full-Stack Authentication with MERN and JWT. Demonstrates robust user authentication using MongoDB, Express.js, React, and Node.js, enhanced with JSON Web Tokens (JWT). Features include secure registration, login, and protected routes, ideal for building secure, scalable web applications.
                     </p>
                 </div>
-            )}
+            </div>
         </div>
     );
 }
